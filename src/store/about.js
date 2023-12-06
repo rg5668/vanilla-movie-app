5ec4a3ff8689a846e96182cdc9b3a3912cc5fa07
kh <rg4093@naver.com>
2023-12-06 22:55:27 +0900

    feat: store

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from '../core';
+
+export default new Store({
+    photo: 'https://kunheelim.com/_next/image?url=%2Fassets%2Fsummer_dog.png&w=256&q=75',
+    name: 'Kunhee',
+    email: 'rg4093@naver.com',
+    blog: 'https://kunheelim.com/',
+    github: 'https://github.com/rg5668',
+    repository: 'https://github.com/rg5668/vanillajs-movie-app',
+});
---
+++ src/store/movie.js
@@ -0,0 +1,58 @@
+import { Store } from '../core';
+
+const store = new Store({
+    searchText: '',
+    page: 1,
+    pageMax: 1,
+    movies: [],
+    movie: {},
+    loading: false,
+    message: 'Search for the movie title!',
+});
+
+export default store;
+
+export const searchMovies = async (page) => {
+    store.state.loading = true;
+    store.state.page = page;
+    if (page === 1) {
+        store.state.movies = [];
+        store.state.message = '';
+    }
+    try {
+        const res = await fetch('/api/movie', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: store.state.searchText,
+                page,
+            }),
+        });
+        const { Search, totalResults, Response, Error } = await res.json();
+        if (Response === 'True') {
+            store.state.movies = [...store.state.movies, ...Search];
+            store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+        } else {
+            store.state.message = Error;
+            store.state.pageMax = 1;
+        }
+    } catch (error) {
+        console.log('searchMovies', error);
+    } finally {
+        store.state.loading = false;
+    }
+};
+
+export const getMovieDetails = async (id) => {
+    try {
+        const res = await fetch('/api/movie', {
+            method: 'POST',
+            body: JSON.stringify({
+                id,
+            }),
+        });
+        const json = await res.json();
+        store.state.movie = json;
+    } catch (error) {
+        console.log('getMovieDetails', error);
+    }
+};
